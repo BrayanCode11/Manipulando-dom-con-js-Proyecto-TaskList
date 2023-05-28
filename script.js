f21e044fa232904448a2ec2ce6e9ef2ce1ba6fc7
brayancode11 <bangarita@unal.edu.co>
2023-05-28 12:28:57 -0500

    clase 04 finalizada

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-const btn = document.querySelector('[data-form-btn]');
+(() => {
+  const btn = document.querySelector('[data-form-btn]');
 
-const createTask = (evento) => {
+  const createTask = (evento) => {
     evento.preventDefault();
     const input = document.querySelector('[data-form-input]');
     const value = input.value;
@@ -9,17 +10,35 @@ const createTask = (evento) => {
     task.classList.add('card');
     input.value = '';
     //backticks
+    const taskContent = document.createElement('div');
 
-    const content =`<div>
-      <i class="far fa-check-square icon"></i>
-      <span class="task">${value}</span>
-    </div>
+    const titleTask = document.createElement('span');
+    titleTask.classList.add('task');
+    titleTask.innerText = value;
+    taskContent.appendChild(checkComplete());
+    taskContent.appendChild(titleTask);
+
+    const content = `
     <i class="fas fa-trash-alt trashIcon icon"></i>`;
-
-    task.innerHTML = content;
-
+    // task.innerHTML = content;
+    task.appendChild(taskContent);
     list.appendChild(task);
-};
+  };
 
-//Arrow functions o funciones anonimas
-btn.addEventListener('click', createTask); 
+  //Arrow functions o funciones anonimas
+  btn.addEventListener('click', createTask);
+
+  const checkComplete = () => {
+    const i = document.createElement('i');
+    i.classList.add('far', 'fa-check-square', 'icon');
+    i.addEventListener('click', completeTask);
+    return i;
+  };
+  // Immediately invoked function expression IIFE
+  const completeTask = (event) => {
+    const element = event.target;
+    element.classList.toggle('fas');
+    element.classList.toggle('completeIcon');
+    element.classList.toggle('far');
+  };
+})();
